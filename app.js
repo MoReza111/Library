9b43945889e0fa9319fa264778a2c7def6a32328
MoReza <mz.abdollahi223@gmail.com>
2020-09-10 01:09:09 +0430

    TEST

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const app = express()
 
 // JSON body Parser
 app.use(express.json())
-app.use(express.static)
-
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
 app.use('/api/v1/books', booksRouter)
 app.use('/api/v1/authors', authorsRouter)
